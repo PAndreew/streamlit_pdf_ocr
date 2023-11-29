0f9c214e3bd5e291825320b0f232e693db8095b1
Andras Pasztor <thepandras@gmail.com>
2023-11-29 22:29:49 +0100

    Remove py2pdf

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pytesseract
 from PIL import Image
-from PyPDF2 import PdfReader
-from pdf2image import convert_from_path
+from pdf2image import convert_from_bytes
 import io
-import tempfile
 
 # Set the title of the app
 st.title('PDF OCR App')
@@ -13,23 +11,13 @@ st.title('PDF OCR App')
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
 if uploaded_file is not None:
-    # Save the uploaded PDF to a temporary file
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as tmpfile:
-        tmpfile.write(uploaded_file.getvalue())
-        pdf_path = tmpfile.name
-
-    # Convert PDF to a list of images
-    pages = convert_from_path(pdf_path, 500)
+    # Convert uploaded PDF to a list of images
+    pages = convert_from_bytes(uploaded_file.read())
 
     # Loop through each page/image
     for i, page_image in enumerate(pages):
-        st.write(f'Page {i+1}')
-
-        # Convert PIL Image to bytes
-        with io.BytesIO() as output:
-            page_image.save(output, format="JPEG")
-            page_image_bytes = output.getvalue()
+        st.write(f'Page {i + 1}')
 
         # Apply OCR to the image
-        page_text = pytesseract.image_to_string(Image.open(io.BytesIO(page_image_bytes)))
+        page_text = pytesseract.image_to_string(page_image)
         st.text_area("Extracted Text", page_text, height=250)
